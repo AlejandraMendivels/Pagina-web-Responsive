6717d01b5251e33aa2dc0ec5a7be55da1df4d10f
Santiago <lspardo@uniboyaca.edu.co>
2024-08-31 04:15:21 -0500

    se corrigio problemas de overflow de pantalla relacionados al menu hamburguesa

--- js/ts/Que_Es.js
+++ js/ts/Que_Es.js
@@ -2,25 +2,30 @@ const buttHamb = document.getElementById("hamburger");
 const navHam = document.getElementById('hamburger_items')
 const body = document.getElementsByTagName('body')
 const cerrar = document.getElementById('cerrar')
+const imgCerrar = document.getElementById('cerrarImg')
 
 buttHamb.addEventListener("click", () => {
+
     navHam.style.visibility = "visible"
-    navHam.style.transition = "all 1s"
     body[0].style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+    navHam.style.width = "60vw";
     navHam.style.marginLeft = "40vw"
     cerrar.style.visibility = "visible"
-    cerrar.style.transition = "all 1s"
-    cerrar.style.left = "90vw"
+    imgCerrar.style.width = "36px"
     
+    navHam.style.animation = ""
+    navHam.style.animationName = ""
 })
 
-cerrar.addEventListener('click',()=>{
-    console.log('hola')
+cerrar.addEventListener('click', () => {
     navHam.style.visibility = "hidden"
-    navHam.style.transition = "all 1s"
-    body[0].style.transition = "all 2s"
+    body[0].style.transition = "all 1s"
     body[0].style.backgroundColor = "initial"
+    imgCerrar.style.width = "0px"
     cerrar.style.visibility = "hidden"
-       cerrar.style.left = "100vw"
-    navHam.style.marginLeft = "100vw"
+    navHam.style.width = "20vw";
+    navHam.style.marginLeft = "80vw"
+    navHam.style.animation = "fadeOut 2s"
+    navHam.style.animationName = "fadeOut"
+
 })
