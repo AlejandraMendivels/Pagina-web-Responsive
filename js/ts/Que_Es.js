5faab6066d7dd5226e64527b1219b1875df231c4
Santiago <lspardo@uniboyaca.edu.co>
2024-09-02 21:55:14 -0500

    se mejoro el menu falta el redireccionamiento a las demas paginas

--- js/ts/Que_Es.js
+++ js/ts/Que_Es.js
@@ -6,26 +6,38 @@ const imgCerrar = document.getElementById('cerrarImg')
 
 buttHamb.addEventListener("click", () => {
 
-    navHam.style.visibility = "visible"
-    body[0].style.backgroundColor = "rgba(0, 0, 0, 0.5)"
-    navHam.style.width = "60vw";
-    navHam.style.marginLeft = "40vw"
-    cerrar.style.visibility = "visible"
-    imgCerrar.style.width = "36px"
-    
-    navHam.style.animation = ""
-    navHam.style.animationName = ""
+  navHam.style.visibility = "visible"
+  body[0].style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+  navHam.style.width = "80vw";
+  navHam.style.marginLeft = "20vw"
+  cerrar.style.visibility = "visible"
+  imgCerrar.style.width = "36px"
+
+  navHam.style.animation = ""
+  navHam.style.animationName = ""
 })
 
 cerrar.addEventListener('click', () => {
-    navHam.style.visibility = "hidden"
-    body[0].style.transition = "all 1s"
-    body[0].style.backgroundColor = "initial"
-    imgCerrar.style.width = "0px"
-    cerrar.style.visibility = "hidden"
-    navHam.style.width = "20vw";
-    navHam.style.marginLeft = "80vw"
-    navHam.style.animation = "fadeOut 2s"
-    navHam.style.animationName = "fadeOut"
+  navHam.style.visibility = "hidden"
+  body[0].style.transition = "all 1s"
+  body[0].style.backgroundColor = "initial"
+  imgCerrar.style.width = "0px"
+  cerrar.style.visibility = "hidden"
+  navHam.style.width = "20vw";
+  navHam.style.marginLeft = "80vw"
+  navHam.style.animation = "fadeOut 2s"
+  navHam.style.animationName = "fadeOut"
 
 })
+
+//dropdown
+
+document.querySelectorAll('.h2-drop').forEach(button => {
+  button.addEventListener('click', () => {
+    const menuId = button.getAttribute('data-target');
+    const menu = document.getElementById(menuId);
+    const arrow = button.querySelector('.arrow-1');
+    arrow.classList.toggle('arrow-1-open')
+    menu.classList.toggle('open');
+  });
+});
